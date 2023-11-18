f052d25fbc2fc4bb0dc116b1d3b845703aa757ae
Minkyeong Jeon <alsrud1228s2@naver.com>
2023-11-18 12:58:44 +0900

    implement vgg16

--- Project2/vgg16_full.py
+++ Project2/vgg16_full.py
@@ -18,12 +18,12 @@ class VGG(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):    # m이 nn.Conv2d인지 확인
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                m.bias.data.zero_()
+                m.weight.data.normal_(0, math.sqrt(2. / n))     # weight을 평균 0, 표준편차 root(2/n)인 정규분포를 따르도록 초기화
+                m.bias.data.zero_()                             # bias 0으로 초기화
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)
+        x = x.view(x.size(0), -1)                               # Flatten
         x = self.classifier(x)
         return x
 
